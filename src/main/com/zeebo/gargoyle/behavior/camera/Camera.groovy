5dbb30d2a6a20e674d9f859280ceea141ca34aa7
Eric Siebeneich <eric.siebeneich@gmail.com>
2015-02-13 13:51:46 -0600

    Uniforms in place

---
+++ src/main/com/zeebo/gargoyle/behavior/camera/Camera.groovy
@@ -0,0 +1,21 @@
+package com.zeebo.gargoyle.behavior.camera
+
+import com.zeebo.gargoyle.behavior.Behavior
+import org.lwjgl.util.vector.Matrix4f
+
+/**
+ * User: Eric
+ */
+class Camera extends Behavior {
+
+	static Camera mainCamera
+
+	ProjectionMatrix projection = new ProjectionMatrix()
+	Matrix4f matrix = new Matrix4f()
+
+	void setMain(boolean value) {
+		if (value) {
+			mainCamera = this
+		}
+	}
+}
---
+++ src/main/com/zeebo/gargoyle/behavior/camera/ProjectionMatrix.groovy
@@ -0,0 +1,42 @@
+package com.zeebo.gargoyle.behavior.camera
+
+import org.lwjgl.util.vector.Matrix4f
+
+/**
+ * User: Eric
+ */
+class ProjectionMatrix {
+
+	Matrix4f matrix
+
+	float fieldOfView
+	float aspectRatio
+	float nearPlane
+	float farPlane
+
+	private boolean dirty = true
+
+	void setProperty(String name, value) {
+		this.@"$name" = value
+		dirty = true
+	}
+
+	def getMatrix() {
+		if (dirty) {
+			def mat = new Matrix4f()
+
+			def frustrumLength = farPlane - nearPlane
+			mat.m11 = 1.0f / Math.tan(Math.toRadians(fieldOfView / 2f))
+			mat.m00 = mat.m11 / aspectRatio
+			mat.m22 = -((farPlane + nearPlane) / frustrumLength)
+			mat.m23 = -1
+			mat.m32 = -((2 * nearPlane * farPlane) / frustrumLength)
+			mat.m33 = 0
+
+			matrix = mat
+			dirty = false
+		}
+
+		return matrix
+	}
+}
